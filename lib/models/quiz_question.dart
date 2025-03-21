241929d27a8068e835810812738d5ae6ad48f885
Rayen Sbai <rayensbai2@gmail.com>
2025-03-22 00:01:38 +0100

    feat: add question model

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -2,20 +2,26 @@ class QuizQuestion {
   const QuizQuestion({
     required this.question,
     required this.answers,
-    this.correctAnswer,
+    required this.correctAnswer,
   });
 
   final String question;
   final List<String> answers;
-  final String? correctAnswer;
+  final List<String> correctAnswer;
 
   bool isAnswerCorrect(String answer) {
-    return answer == correctAnswer;
+    return correctAnswer.contains(answer);
   }
 
   List<String> get shuffledAnswers {
     final shuffled = List<String>.from(answers);
     shuffled.shuffle();
     return shuffled;
+  }
+
+  static List<QuizQuestion> getShuffledQuestions(List<QuizQuestion> questions) {
+    final shuffledQuestions = List<QuizQuestion>.from(questions);
+    shuffledQuestions.shuffle();
+    return shuffledQuestions;
   }
 }
